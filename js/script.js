b47bb14d307529d7a4223962c91c137f0e52a3a0
walterhenrique <56816256+walterhenrique@users.noreply.github.com>
2021-09-09 12:35:28 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
  //document.write "essa mensagem foi gerada por JS"//
+ var conteudopostagem = "";
+ function postar() {
+     conteudopostagem = window.document.getElementById("postagem").value;
+     window.document.getElementById("postrecente").innerHTML = conteudopostagem;
+ }
