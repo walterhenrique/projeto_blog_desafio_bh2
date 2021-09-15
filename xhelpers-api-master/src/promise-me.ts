5c8e4ddfbfcdbf107f507ec2d682996bf64ba2ee
walterhenrique <56816256+walterhenrique@users.noreply.github.com>
2021-09-15 17:06:24 -0300

    Delete xhelpers-api-master directory

--- xhelpers-api-master/src/promise-me.ts
+++
@@ -1,7 +0,0 @@
-export const promiseMe = async (promise: any): Promise<[any, any]> => {
-  return promise
-    .then((data: any) => [data, undefined])
-    .catch((error: any) => {
-      return Promise.resolve([undefined, error]);
-    });
-}
